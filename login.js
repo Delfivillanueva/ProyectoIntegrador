acfe63ba9c131f2ed8079b1d1a6e5cac23247d36
esperanza uranga <euranga@udesa.edu.ar>
2019-05-31 10:29:50 -0300

    login js

---
+++ login.js
@@ -0,0 +1,42 @@
+window.onload = function () {
+
+// como hago para q el usuario quede logueado
+
+
+
+// si el usuario ya hizo log in cambiar ingresar x su nombre
+// y agregar el boton de peliculas favoritas
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

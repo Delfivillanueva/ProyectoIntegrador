b4f71a6cf116c47302f99735065eb90a6a482a01
esperanza uranga <euranga@udesa.edu.ar>
2019-05-31 13:48:45 -0300

    home js

--- home.js
+++ home.js
@@ -1,40 +1,19 @@
 window.onload = function () {
+  console.log(localStorage);
+    var usuario = localStorage.getItem("usuario")
+    if (usuario.length>0) {
+      console.log("entramos al if");
+      var ingresar = document.querySelector("a.login")
+      var usuario = document.querySelector("p.nameUsuario")
+      var pelisFavs = document.querySelector ("a.pelisPrefes")
+      //console.log(ingresar);
+      //console.log(usuario);
+      //console.log(pelisFavs);
 
-  var usu = localStorage.getItem("usuario")
-  if (usu !=null) {
-    var ingresar = document.querySelector("a.login")
-    var usuario = document.querySelector("")
-    var pelisFavs = document.querySelector ("")
-    ingresar.style.display = block,
-    usuario.style.display = none,
-    pelisFavs.style.display = none,
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //  ingresar.style.display = block;
+    //  usuario.style.display = block;
+    //  pelisFavs.style.display = block;
+    }
 
 
 

5f3bd99b286dae69593ed1af5357dad8fae15b5a
esperanza uranga <euranga@udesa.edu.ar>
2019-05-31 10:54:40 -0300

    login.js

--- login.js
+++ login.js
@@ -7,10 +7,12 @@ window.onload = function () {
 // si el usuario ya hizo log in cambiar ingresar x su nombre
 // y agregar el boton de peliculas favoritas
 if (el usuario ya hico log in) {
-  var ingresar = document.querySelector("a.login"),
-  var usuario = document.querySelector(""),
+  var ingresar = document.querySelector("a.login")
+  var usuario = document.querySelector("")
+  var pelisFavs
   ingresar.style.display = block,
   usuario.style.display = none,
+
 }
 
 

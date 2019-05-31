1efc253ce0f81fa9c8f80d241fe7055c00bc4d69
esperanza uranga <euranga@udesa.edu.ar>
2019-05-31 11:48:58 -0300

    iniciar sesion js

---
+++ home.js
@@ -0,0 +1,41 @@
+window.onload = function () {
+
+  var usu = localStorage.getItem("usuario")
+  if (usu !=null) {
+    var ingresar = document.querySelector("a.login")
+    var usuario = document.querySelector("")
+    var pelisFavs = document.querySelector ("")
+    ingresar.style.display = block,
+    usuario.style.display = none,
+    pelisFavs.style.display = none,
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- login.js
+++ login.js
@@ -1,53 +1,25 @@
 window.onload = function () {
+  //var usuarioLogueado = localStorage.getItem("usuario")
 
-iniciarSesion.onclick = function (a) {
-  a.preventDefault;
-  var valor = document.querySelector("input.usuario").value
-  if (...) {
-    localStorage.setItem("usuario" + valor)
+  //if (usuarioLogueado != null) {
+    //location.href = "home.html"
+  //}
+
+
+  //console.log(localStorage);
+  var iniciarSesion = document.querySelector("a.iniciarSesion")
+  iniciarSesion.onclick = function (a) {
+    a.preventDefault();
+    var valor = document.querySelector("input.usuario").value
+    var usuario = document.querySelector("input.usuario")
+    var mail = document.querySelector("input.mail")
+    var genero = document.querySelector("input.genero")
+
+
+    if (usuario!= null, mail!= null, genero!= null) {
+      localStorage.setItem("usuario" , valor)
+
+    }
+
   }
-}
-
-var usu = localStorage.getItem("usuario")
-if (usu !=null) {
-  var ingresar = document.querySelector("a.login")
-  var usuario = document.querySelector("")
-  var pelisFavs = document.querySelector ("")
-  ingresar.style.display = block,
-  usuario.style.display = none,
-  pelisFavs.style.display = none,
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }

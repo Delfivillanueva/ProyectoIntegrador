9c2f5072b9437440075235e885fba9e07a9bf990
Delfi <villandelfi@gmail.com>
2019-05-31 14:19:14 -0300

    peliculas por genero falta arrgelar img

--- Peliculasporgen.js
+++ Peliculasporgen.js
@@ -10,11 +10,28 @@ var genreURL = "https://api.themoviedb.org/3/discover/movie?api_key=5d02a3447f4e
   .then(function(response){
     return response.json();
   })
-  .then (function(data){
-     console.log(data);
 
-
+  .then (function (data) {
+    console.log(data);
+    var h1= document.querySelector("body h1");
+    h1.innerHTML+=nombre;
+    var ul = document.querySelector("body ul");
+    var li = "";
+    var arrayDePeliculas= data.results
+    var array=0;
+    for (var i = 0; i < arrayDePeliculas.length; i++) {
+      title = arrayDePeliculas[i].title;
+      id= arrayDePeliculas[i].id;
+      poster= arrayDePeliculas[i].poster_path
+      li = "<li>"
+      li += "<p><a href='detallePeliculas.html?id="+ id + "'>" + title + "</a></p>" + "img src='https://image.tmdb.org/t/p/w500" + poster +"'>"
+      li += "</li>"
+      ul.innerHTML+=li
+    }
   })
+
+
+
   .catch(function(error){
     console.log("Error"+ error);
   })

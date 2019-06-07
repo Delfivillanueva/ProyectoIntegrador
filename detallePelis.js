6946609a4e54951c929f6d946b78d027590b046c
esperanza uranga <euranga@udesa.edu.ar>
2019-06-07 10:45:12 -0300

    recomenaciones

--- detallePelis.js
+++ detallePelis.js
@@ -72,41 +72,45 @@ fetch(url)
 
   // IDEA: RECOMENDACIONES
 
-  var url = "https://api.themoviedb.org/3/movie/{movie_id}/recommendations?api_key=5d02a3447f4e9a0a8eaf7b743846e766&language=en-US&page=1"
+  var QueryString = new URLSearchParams(location.search)
+  var id= QueryString.get("id")
+
+  var url = "https://api.themoviedb.org/3/movie/" + id + "/recommendations?api_key=5d02a3447f4e9a0a8eaf7b743846e766&language=en-US&page=1"
+    fetch (url)
     .then(function(response){
       return response.json();
     })
     .then (function(data){
-       console.log(data);
-       //console.log(data.results);
+       // console.log(data);
+       console.log(data.results);
 
-       // var ul = document.querySelector("ul.peliculasmaspun");
-       // var li = "";
-       // var p =  "";
-       // var a = "";
-       // var arrayDePelis = data.results;
-       // var peli = "";
-       // var title = "";
-       // var poster = "";
-       // var id = 0
-       // var urlImagen = "https://image.tmdb.org/t/p/original";
-       //
-       // for (var i = 0; i < arrayDePelis.length; i++) {
-       //   peli = arrayDePelis[i]
-       //   title = peli.title
-       //   poster = peli.poster_path
-       //   id = peli.id
-       //
-       //   a = "<a href='detallePelis.html?id="+ id + "'>"
-       //     a += "<img src='" + urlImagen + poster + "'>"
-       //     a += "<p>" + title + "</p>"
-       //   a += "</a>"
-       //
-       //   ul.innerHTML += "<li>" + a + "</li>"
+       var ul = document.querySelector("ul");
+       var li = "";
+       var p =  "";
+       var a = "";
+       var arrayDePelis = data.results;
+       var peli = "";
+       var title = "";
+       var poster = "";
+       var id = 0
+       var urlImagen = "https://image.tmdb.org/t/p/original";
 
-         //console.log(peli.title);
-         //console.log(peli.poster_path);
-       //}
+       for (var i = 0; i < arrayDePelis.length; i++) {
+         peli = arrayDePelis[i]
+         title = peli.title
+         poster = peli.poster_path
+         id = peli.id
+
+         a = "<a href='detallePelis.html?id="+ id + "'>"
+           a += "<img src='" + urlImagen + poster + "'>"
+           a += "<p>" + title + "</p>"
+         a += "</a>"
+
+         ul.innerHTML += "<li>" + a + "</li>"
+
+         // console.log(peli.title);
+         // console.log(peli.poster_path);
+       }
 
     })
     .catch(function(error){

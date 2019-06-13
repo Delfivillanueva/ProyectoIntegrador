7b40cdfd0812044cc8bf34cff2e1ca0db6b7e40f
Delfi <villandelfi@gmail.com>
2019-06-13 16:30:04 -0300

    cambie de lugar la function

--- encabezadolog.js
+++ encabezadolog.js
@@ -12,15 +12,16 @@ window.addEventListener("load",function(){
 
       usuarioHeader.innerHTML += "<p>" + usuario + "</p>"
     }
-    function menuFunction() {
-      var b = document.querySelector("form.buscador");
-      var x = document.getElementById("myLinks");
-      if (x.style.display === "block") {
-        x.style.display = "none";
-        b.style.display = "block";
-      } else {
-        x.style.display = "block";
-         b.style.display = "none";
-      }
-    }
+
 })
+function menuFunction() {
+  var b = document.querySelector("form.buscador");
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    b.style.display = "block";
+  } else {
+    x.style.display = "block";
+     b.style.display = "none";
+  }
+}

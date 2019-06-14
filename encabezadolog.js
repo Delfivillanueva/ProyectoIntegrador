19ab2dc88093acae5388f77f9a36db5f98a441b6
Delfi <villandelfi@gmail.com>
2019-06-14 09:52:36 -0300

    Centrado de fotos y titlos en las pags

--- encabezadolog.js
+++ encabezadolog.js
@@ -2,15 +2,20 @@ window.addEventListener("load",function(){
   console.log(localStorage);
     var usuario = localStorage.getItem("usuario")
     if (usuario.length>0) {
-      var ingresar = document.querySelector("a.login")
+      var ingresarGrande = document.querySelector("a.login")
+      var ingresarMobile = document.querySelector("div#myLinks a.login")
       var usuarioHeader = document.querySelector("p.nameUsuario")
+      var usuarioMobile = document.querySelector("p.nameUsuarioMobile")
       var pelisFavs = document.querySelector ("a.pelisPrefes")
 
-      ingresar.style.display = "none";
+      ingresarGrande.style.display = "none";
+      ingresarMobile.style.display = "none";
       usuarioHeader.style.display = "block";
+      usuarioMobile.style.display = "block";
       pelisFavs.style.display = "block";
 
       usuarioHeader.innerHTML += "<p>" + usuario + "</p>"
+      usuarioMobile.innerHTML += "<p>" + usuario + "</p>"
     }
 
 })
